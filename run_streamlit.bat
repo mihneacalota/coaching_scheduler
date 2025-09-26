cb56f7313d4e7a002631291055f8606ff061486b
Calota <70659341+mihneacalota@users.noreply.github.com>
2025-09-26 08:44:46 +0200

    Update run_streamlit.bat

--- run_streamlit.bat
+++ run_streamlit.bat
@@ -1,6 +1,6 @@
 @echo off
-REM Navigate to the directory containing the Streamlit app
-cd /d "c:\Users\Mihnea\Desktop\Coding and Git\PhD\ARTIFICE_Scheduler\coaching_scheduler"
+@REM REM Navigate to the directory containing the Streamlit app
+@REM cd /d "c:\Users\Mihnea\Desktop\Coding and Git\PhD\ARTIFICE_Scheduler\coaching_scheduler"
 
 REM Run the Streamlit app
 streamlit run coaching_plan.py
